33d29f77e138232f666be9fa3f0875457ed5f0c8
Papa1999 <thioubepapamacoumba@gmail.com>
2024-12-31 17:34:27 +0100

    First Course about next JS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
+              src/pages/index.tsx
             </code>
             .
           </li>
